a09256b1f87859a4dff9d596c9efb643877f19e6
Josiane Alves <josianealves.ads@gmail.com>
2025-10-02 19:07:26 -0300

    feat: Adiciona código de variáveis e tipos de dados.

---
+++ 02_Estruturas_Condicionais/02_Estruturas_Condicionais/01_calculo_media_e_situacao.py
@@ -0,0 +1,18 @@
+# --- ENTRADA DE DADOS E CONVERSÃO (CASTING) ---
+
+print("--- Calculadora de Média e Situação do Aluno ---")
+print("Digite a primeira nota:")
+nota_1 = int(input()) # <--- Essencial para demonstrar a habilidade
+
+print("Digite a segunda nota:")
+nota_2 = int(input())
+
+# ... e assim por diante ...
+
+# --- ESTRUTURA CONDICIONAL (IF/ELSE) ---
+if media >= 6:
+    situacao = 'Aprovado'
+else:
+    situacao = 'Reprovado'
+
+# ...
