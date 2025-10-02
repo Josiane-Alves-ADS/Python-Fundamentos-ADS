bdce654aed3b4f17e71a12016b7d75dc986aa150
Josiane Alves <josianealves.ads@gmail.com>
2025-10-02 20:44:03 -0300

    feat: Adiciona script interativo de cinema com múltiplas condições.

--- 02_Estruturas_Condicionais/02_Estruturas_Condicionais/02_Estruturas_Condicionais/02_faixa_etaria_elif.py
+++ 02_Estruturas_Condicionais/02_Estruturas_Condicionais/02_Estruturas_Condicionais/02_faixa_etaria_elif.py
@@ -1,21 +1,12 @@
-# --- MÁQUINA DE VENDA DE INGRESSOS DE CINEMA ---
+# --- CLASSIFICAÇÃO DE FAIXA ETÁRIA COM ELIF ---
 
-# 1. Solicita a idade do cliente
-# O input é necessário para demonstrar a interatividade e o casting de tipos.
-idade = int(input("Por favor, digite sua idade: "))
+# Atribuição da variável (pode ser alterada pelo usuário)
+idade = 25
 
-# 2. Sugestão de filmes baseada na faixa etária
-if idade < 12:
-    print("Recomendamos o filme infantil 'Toy Story'.")
-elif 12 <= idade < 18: # Condição combinada para a faixa adolescente
-    print("Recomendamos o filme adolescente 'Harry Potter e a Pedra Filosofal'.")
+# Estrutura condicional aninhada (if/elif/else)
+if idade < 18:
+    print("Menor de idade")
+elif idade < 65:  # Já sabemos que é >= 18
+    print("Adulto")
 else:
-    print("Recomendamos o emocionante 'O Poderoso Chefão'.")
-    
-# 3. Verifica a disponibilidade de ingressos (uma condição separada)
-quantidade_ingressos = 10 
-
-if quantidade_ingressos > 0:
-    print("Ingressos estão disponíveis. Divirta-se no cinema!")
-else:
-    print("Desculpe, todos os ingressos estão esgotados para hoje.")
+    print("Idoso")
