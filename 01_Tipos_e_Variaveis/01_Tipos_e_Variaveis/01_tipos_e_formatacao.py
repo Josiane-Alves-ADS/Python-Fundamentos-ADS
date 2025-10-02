098064fd3c9a34e9f12d70fc83de20050be85f22
Josiane Alves <josianealves.ads@gmail.com>
2025-10-02 19:08:42 -0300

    feat: Adiciona estrutura inicial e códigos do Módulo 1 (Aula 1)

--- 01_Tipos_e_Variaveis/01_Tipos_e_Variaveis/01_tipos_e_formatacao.py
+++ 01_Tipos_e_Variaveis/01_Tipos_e_Variaveis/01_tipos_e_formatacao.py
@@ -1,1 +1,15 @@
+# Demonstração de atribuição de variáveis e tipos de dados
+x = 10
+nome = 'aluno'
+nota = 8.75
+fez_inscricao = True
 
+# Imprimindo os tipos de dados
+print(f"O tipo de x (valor {x}) é: {type(x)}")
+print(f"O tipo de nome (valor {nome}) é: {type(nome)}")
+print(f"O tipo de nota (valor {nota}) é: {type(nota)}")
+print(f"O tipo de fez_inscricao (valor {fez_inscricao}) é: {type(fez_inscricao)}")
+
+# Exemplo de formatação de string (f-string)
+nome_aluno = "Josiane"
+print(f"\nOlá, {nome_aluno}, bem-vindo à disciplina de programação.")
