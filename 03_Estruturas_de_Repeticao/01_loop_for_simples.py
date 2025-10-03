40517df9f384633b28722bd6c2896b05b18a20ae
Josiane Alves <josianealves.ads@gmail.com>
2025-10-02 21:09:10 -0300

    feat: Adiciona demonstração de loop for simples em listas.

---
+++ 03_Estruturas_de_Repeticao/01_loop_for_simples.py
@@ -0,0 +1,15 @@
+# --- ESTRUTURA DE REPETIÇÃO FOR SIMPLES ---
+
+# Itera sobre uma lista de números
+numeros = [1, 2, 3, 4, 5]
+
+for numero in numeros:
+    print(numero)
+    
+print("-" * 10)
+
+# Itera sobre uma lista de strings
+filmes = ["Chuck", "Meu Malvado Favorito", "Super Mario"]
+
+for filme in filmes:
+    print(f"Filme: {filme}")
