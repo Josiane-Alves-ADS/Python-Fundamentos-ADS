f1b7fd8d08e8c1634d0c7cd659cd5d077ee4bfc9
Josiane Alves <josianealves.ads@gmail.com>
2025-10-02 21:00:50 -0300

    feat: Adiciona script interativo de cinema com múltiplas condições.

---
+++ 02_Estruturas_Condicionais/02_Estruturas_Condicionais/02_Estruturas_Condicionais/03_cinema_condicionais_complexas.py
@@ -0,0 +1,20 @@
+# --- MÁQUINA DE VENDA DE INGRESSOS DE CINEMA ---
+
+# 1. Solicita a idade do cliente
+idade = int(input("Por favor, digite sua idade: "))
+
+# 2. Sugestão de filmes baseada na faixa etária
+if idade < 12:
+    print("Recomendamos o filme infantil 'Toy Story'.")
+elif 12 <= idade < 18:
+    print("Recomendamos o filme adolescente 'Harry Potter e a Pedra Filosofal'.")
+else:
+    print("Recomendamos o emocionante 'O Poderoso Chefão'.")
+
+# 3. Verifica a disponibilidade de ingressos (uma condição separada)
+quantidade_ingressos = 10 
+
+if quantidade_ingressos > 0:
+    print("Ingressos estão disponíveis. Divirta-se no cinema!")
+else:
+    print("Desculpe, todos os ingressos estão esgotados para hoje.")
